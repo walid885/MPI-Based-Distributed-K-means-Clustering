84004c26c49e4e8ec4ead73664652178c52bd06f
walid885 <bettaieb88walid@gmail.com>
2025-03-29 00:57:00 +0100

    setting up the path and MPI using python correctly , being able to do so , here is the link of the tutorial to be able to document it in blogs , https://www.youtube.com/watch?v=bkfCrj-rBjU

--- src/init.py
+++ src/init.py
@@ -1,3 +1,3 @@
 from mpi4py import MPI
-print(f"MPI initialized successfully. Rank: {MPI.COMM_WORLD.Get_rank()}, Size: {MPI.COMM_WORLD.Get_size()}")
+print(f"MPI initialized successfully. and being able to get the path correctly  Rank: {MPI.COMM_WORLD.Get_rank()}, Size: {MPI.COMM_WORLD.Get_size()}")
 

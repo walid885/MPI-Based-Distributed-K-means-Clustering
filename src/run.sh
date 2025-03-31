926ff3ff3bbc956de3e7784e4b0bd6c7727cd072
walid885 <bettaieb88walid@gmail.com>
2025-03-31 12:57:30 +0100

    adding the run.sh script

--- src/run.sh
+++ src/run.sh
@@ -12,7 +12,7 @@ echo -e "${BLUE}Starting the MPI-Based Distributed K-means Clustering process...
 
 # Run dataCleaning.py
 echo -e "${YELLOW}Running data cleaning script...${NC}"
-python3 ./src/dataCleaning.py
+python3 dataCleaning.py
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}Data cleaning completed successfully!${NC}"
 else
@@ -20,9 +20,29 @@ else
     exit 1
 fi
 
+# Run kmeans_sequential.py
+echo -e "${YELLOW}Running sequential K-means clustering script...${NC}"
+python3 kmeans_sequantial.py
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Sequential K-means clustering completed successfully!${NC}"
+else
+    echo -e "${RED}Sequential K-means clustering script encountered an error.${NC}"
+    exit 1
+fi
+
+# Run kmeans_parallel.py
+echo -e "${YELLOW}Running parallel K-means clustering script...${NC}"
+python3 kmeans_parallel.py
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Parallel K-means clustering completed successfully!${NC}"
+else
+    echo -e "${RED}Parallel K-means clustering script encountered an error.${NC}"
+    exit 1
+fi
+
 # Run Comparison.py
 echo -e "${YELLOW}Running comparison script...${NC}"
-python3 ./src/Comparison.py
+python3 Comparison.py
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}Comparison completed successfully!${NC}"
 else
